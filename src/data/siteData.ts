82e4fcfb2387ea23019c9257bc0a82094585b2ed
Andres Eduardo RA <alpiryk@gmail.com>
2025-12-22 21:22:48 -0400

    add map

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -5,6 +5,8 @@ export const siteData = {
     phone: "+591 70930853",
     phoneLink: "tel:+59170930853",
     address: "Santa cruz, Bolivia", // Placeholder
+    mapUrl:
+      "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d949.7858207497464!2d-63.18823722499657!3d-17.784961910406324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sbo!4v1766452831368!5m2!1ses-419!2sbo",
     slogan:
       "Impulsamos el crecimiento de tu negocio con soluciones digitales innovadoras.",
     description:
